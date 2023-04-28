26a5fc3eb2a6dacbf889afa146d88623dd906570
Rossen <Rgotsev6164@stu.d214.org>
2023-04-28 12:31:55 -0500

    basic outline

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            VStack{
+                VStack{
+                    Text("*Pinned Stocks")
+                    
+                    Spacer()
+                        .frame(height: 250)
+                    Divider()
+                       
+                    Text("*Big Movers")
+                }
+                
+                
+                    .foregroundColor(.blue)
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarLeading){   
+                            TextField("Search Stocks", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                                .textFieldStyle(.roundedBorder)
+                                .frame(minWidth: 200, idealWidth: 290, maxWidth: 390, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        }
+                        ToolbarItem(placement: .navigationBarTrailing){
+                            Button("News") {
+                                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                            }
+                        }
+                        
+                        
+                        }
+                
+                
+                    }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.white)
+            }
         }
     }
-}
+
