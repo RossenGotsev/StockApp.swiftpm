97c79398e361be9efb13f38036b575cda0685c27
Kazuya Nishizaka <knishizaka6294@stu.d214.org>
2023-05-02 13:22:20 -0500

    Added notification button that takes user to notification view.

--- ContentView.swift
+++ ContentView.swift
@@ -5,9 +5,9 @@ struct ContentView: View {
     @State var url = URL(string: "")
     @State var StockSearch = ""
     let pinnedStocks = [
-           Stock(symbol: "APPL", price: 168.75),
-           Stock(symbol: "MSFT", price: 305.66),
-           Stock(symbol: "NVDA", price: 275.67)]
+        Stock(symbol: "APPL", price: 168.75),
+        Stock(symbol: "MSFT", price: 305.66),
+        Stock(symbol: "NVDA", price: 275.67)]
     let BigMoversStocks = [
         Stock(symbol: "UBER", price: 36.29),
         Stock(symbol: "MAR", price: 177.88),
@@ -22,7 +22,7 @@ struct ContentView: View {
                         HStack{
                             VStack{
                                 Text(pinnedStocks.symbol)
-                                    Text("$\(pinnedStocks.price, specifier: "%.2f")")
+                                Text("$\(pinnedStocks.price, specifier: "%.2f")")
                             }
                             Spacer()
                             Button {
@@ -34,10 +34,10 @@ struct ContentView: View {
                         }
                     }
                     Spacer()
-//                        .frame(height: 250)
+                    //                        .frame(height: 250)
                     Divider()
                     Text("*Big Movers")
-                
+                    
                 }
                 List(BigMoversStocks){ BigMoversStocks in
                     HStack{
@@ -47,37 +47,45 @@ struct ContentView: View {
                         }
                         Spacer()
                         Button {
-
+                            
                         } label: {
                             Image(systemName: "pin")
                                 .font(.title)
                         }
                     }
                 }
-                    .foregroundColor(.blue)
-                    .toolbar {
-                        ToolbarItem(placement: .navigationBarLeading){   
-                            TextField("Search Stocks", text: $StockSearch)
-                                .textFieldStyle(.roundedBorder)
-                                .frame(minWidth: 200, idealWidth: 290, maxWidth: 390)
-                        }
-                        ToolbarItem(placement: .navigationBarTrailing){
-                            NavigationLink("Search"){
-                                StockView()
-                            }
-                            .onTapGesture {
-                                url=URL(string: "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=\(StockSearch)&interval=5min&apikey=JUHAWMR1G46CJSY9")!
-                            }
+                .foregroundColor(.blue)
+                .toolbar{
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        NavigationLink {
+                            NotificationView()
+                        } label: {
+                            Label("notification", systemImage: "bell")
                         }
                         
-                        
-                        }
-                
-                
                     }
+                    ToolbarItem(placement: .navigationBarLeading){
+                        TextField("Search Stocks", text: $StockSearch)
+                            .textFieldStyle(.roundedBorder)
+                            .frame(minWidth: 200, idealWidth: 290, maxWidth: 390)
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing){
+                        NavigationLink("Search"){
+                            StockView()
+                        }
+                        .onTapGesture {
+                            url=URL(string: "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=\(StockSearch)&interval=5min&apikey=JUHAWMR1G46CJSY9")!
+                        }
+                    }
+                    
+                    
+                }
+                
+                
+            }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(.white)
-            }
         }
     }
+}
 
